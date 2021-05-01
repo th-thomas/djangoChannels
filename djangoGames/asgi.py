c9385bcf1ce3f8b08c7130bad9fe39b716dd7889
Thibault Thomas <thibaulthomas@gmail.com>
2021-05-01 19:19:19 +0200

    Pointed the root routing configuration at the chat.routing module

--- djangoGames/asgi.py
+++ djangoGames/asgi.py
@@ -1,11 +1,17 @@
 import os
-
-from channels.routing import ProtocolTypeRouter
+import chat.routing
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoGames.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
-    # Just HTTP for now. (We can add other protocols later.)
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
+    ),
 })
